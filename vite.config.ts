e0d4aeb778f36e6d71744e5bbe11f4240b8e794e
fasty86 <oleg.tolm1986@gmail.com>
2024-12-03 12:09:30 +0300

    feat: инициализация проекта и конфигурационных файлов

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  css: {
+    postcss: './postcss.config.ts',
+  },
+});
