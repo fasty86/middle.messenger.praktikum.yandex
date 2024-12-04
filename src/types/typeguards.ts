a2cf274e34b28c1e0c358366fedc968168b9069c
fasty86 <oleg.tolm1986@gmail.com>
2024-12-04 14:18:17 +0300

    feat: добавлены typeguard'ы

---
+++ src/types/typeguards.ts
@@ -0,0 +1,6 @@
+export function isAnchorElement(element: any): element is HTMLAnchorElement {
+  return element instanceof HTMLAnchorElement;
+}
+export function isButtonElement(element: any): element is HTMLButtonElement {
+  return element instanceof HTMLButtonElement;
+}
