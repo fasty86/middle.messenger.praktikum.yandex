35e07c80ed270a25dd8ff7001aa999ef05b62230
fasty86 <oleg.tolm1986@gmail.com>
2024-12-06 19:22:40 +0300

    feat:  в компонент меню добавлен компанент кнопки

--- src/components/menu/Menu.ts
+++ src/components/menu/Menu.ts
@@ -6,6 +6,6 @@ export default `<div class="options-group">
                      {{> Option data=data.items }}
                     {{/each}}
                 </div>
-                {{> Button }}
-                <button class="chat-area__header-options "></button>
-              </div>`;
+                 {{> Button id=data.optionButton.id type=data.optionButton.type text=data.optionButton.text disabled=data.optionButton.disabled className=data.optionButton.className}}
+              </div>
+             `;
