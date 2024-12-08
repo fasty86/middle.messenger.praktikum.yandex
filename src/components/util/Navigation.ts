3f7353618183568f9b3c9f8033dfab6a08ca09b3
fasty86 <oleg.tolm1986@gmail.com>
2024-12-08 15:53:52 +0300

    fix: список ссылок для навигации обернут в тэги согласно семантике

--- src/components/util/Navigation.ts
+++ src/components/util/Navigation.ts
@@ -1,10 +1,12 @@
 export default `<nav class="util_nav">
-    {{> Link href="/login" text="логин" className=""}}
-    {{> Link href="/registration" text="регистрация" className=""}}
-    {{> Link href="/chat" text='чат' className=""}}
-    {{> Link href="/profile" text='профиль' className=""}}
-    {{> Link href="/profile/edit/data" text='изменение данных профиля' className=""}}
-    {{> Link href="/profile/edit/password" text='изменение пароля' className=""}}
-    {{> Link href="/notFound" text="404" className=""}}
-    {{> Link href="/serverError" text="500" className=""}}
+<ul>
+   <li> {{> Link href="/login" text="логин" className=""}}</li>
+    <li>{{> Link href="/registration" text="регистрация" className=""}}</li>
+    <li>{{> Link href="/chat" text='чат' className=""}}</li>
+    <li>{{> Link href="/profile" text='профиль' className=""}}</li>
+    <li>{{> Link href="/profile/edit/data" text='изменение данных профиля' className=""}}</li>
+    <li>{{> Link href="/profile/edit/password" text='изменение пароля' className=""}}</li>
+    <li>{{> Link href="/notFound" text="404" className=""}}</li>
+    <li>{{> Link href="/serverError" text="500" className=""}}</li>
+     </ul>
 </nav>`;
