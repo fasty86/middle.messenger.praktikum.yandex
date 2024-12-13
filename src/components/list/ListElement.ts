db69bd44c8548e550f1658d075f763c5cbaefb89
fasty86 <oleg.tolm1986@gmail.com>
2024-12-13 23:23:33 +0300

    feat: Добавлен компонент элемента списка

---
+++ src/components/list/ListElement.ts
@@ -0,0 +1,12 @@
+import "./listelement.pcss";
+import Block from "../../framework/Block";
+import { PropsType } from "../../framework/types";
+export default class ListElement extends Block {
+    constructor(props: PropsType) {
+        super(props);
+    }
+
+    render() {
+        return `<li class="{{className}}">{{{content}}}</li>`;
+    }
+}
