0746bbf831b3caef1fc12ac9037e2f41fe25c134
fasty86 <oleg.tolm1986@gmail.com>
2024-12-13 15:41:39 +0300

    feat: добавлены функции

--- src/types/typeguards.ts
+++ src/types/typeguards.ts
@@ -1,6 +1,16 @@
-export function isAnchorElement(element: any): element is HTMLAnchorElement {
-  return element instanceof HTMLAnchorElement;
+export function isAnchorElement(
+    element: HTMLElement,
+): element is HTMLAnchorElement {
+    return element instanceof HTMLAnchorElement;
 }
-export function isButtonElement(element: any): element is HTMLButtonElement {
-  return element instanceof HTMLButtonElement;
+export function isButtonElement(
+    element: HTMLElement,
+): element is HTMLButtonElement {
+    return element instanceof HTMLButtonElement;
+}
+export function isFunction(prop: unknown): prop is Function {
+    return typeof prop === "function";
+}
+export function isHTMLElement(element: unknown): element is HTMLElement {
+    return element instanceof HTMLElement;
 }
