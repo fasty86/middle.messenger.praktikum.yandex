b54c98d74c5aa5fcc7280a35f594e8415a7b0143
fasty86 <oleg.tolm1986@gmail.com>
2024-12-17 13:26:45 +0300

    feat: добавлены вспомогательные функции

---
+++ src/utils/converters.ts
@@ -0,0 +1,8 @@
+export function capitalize(str: unknown) {
+    const newStr = new String(str);
+    if (newStr instanceof String && newStr !== undefined) {
+        const capitalized = newStr.charAt(0).toUpperCase() + newStr.slice(1);
+        return capitalized;
+    }
+    return str;
+}
